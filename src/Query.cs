0383051e61301bd468ed13d8666014b7a4934033
Jamess-Lucass <jameslucas1210@gmail.com>
2024-01-18 11:58:37 +0000

    wip

--- src/Query.cs
+++ src/Query.cs
@@ -1,8 +1,8 @@
 public record Query
 {
-    public int? Top { get; set; } = 0;
-    public int? Skip { get; set; } = 0;
-    public bool? Count { get; set; } = false;
+    public int? Top { get; set; }
+    public int? Skip { get; set; }
+    public bool? Count { get; set; }
     public string? OrderBy { get; set; } = string.Empty;
     public string? Select { get; set; } = string.Empty;
     public string? Search { get; set; } = string.Empty;
